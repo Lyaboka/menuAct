642ed6fccc4af06c93b383e3f15ef627a14c83ee
Lyaboka <thelacoste2000@gmail.com>
2022-02-05 13:51:40 +0600

    fix submenu

--- app/src/main/java/com/example/basicactivity/MenuActivity.kt
+++ app/src/main/java/com/example/basicactivity/MenuActivity.kt
@@ -50,7 +50,7 @@ class MenuActivity : AppCompatActivity() {
         popupMenu.inflate(R.menu.popupmenu)
         popupMenu.show()
 
-        popupMenu.setOnMenuItemClickListener{
+        popupMenu.setOnMenuItemClickListener{ it -> R.id.menugroup1
             when (it.itemId) {
                 R.id.menu1 -> {
                     helloTV.text = "You choose Popup 1"
@@ -59,20 +59,32 @@ class MenuActivity : AppCompatActivity() {
                     helloTV.text = "You choose Popup 2"
                 }
                 R.id.menu3 -> {
-                    when (it.itemId){
-                        R.id.submenu ->{
-                            helloTV.text = "You choose Submenu 1"
+                    popupMenu.setOnMenuItemClickListener {it -> R.id.submenugroup
+                        when (it.itemId) {
+                            R.id.submenu -> {
+                                helloTV.text = "You choose Submenu 1"
+                            }
+                            R.id.submenu2 -> {
+                                helloTV.text = "You choose Submenu 2"
+                            }
+                            R.id.submenu3 -> {
+                                helloTV.text = "You choose Submenu 3"
+                            }
                         }
-                        R.id.submenu2 ->{
-                            helloTV.text = "You choose Submenu 2"
-                        }
-                        R.id.submenu3 ->{
-                            helloTV.text = "You choose Submenu 3"
-                        }
+                        true
                     }
                 }
-            }
-            true
+                R.id.menu4 -> {
+                    helloTV.text = "You choose Popup 4"
+                }
+                R.id.menu5 -> {
+                    helloTV.text = "You choose Popup 5"
+                }
+                R.id.menu6 -> {
+                    helloTV.text = "You choose Popup 6"
+                }
+                }
+                true
         }
 
     }
